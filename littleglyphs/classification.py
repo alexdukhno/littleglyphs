ab760eb0b3891add5b2ed701d7e39e3838353747
alexdukhno <alexdukhno@users.noreply.github.com>
2019-11-13 15:49:20 +0100

    Add functionality for classification using Siamese networks (currently CNN-based)

--- littleglyphs/classification.py
+++ littleglyphs/classification.py
@@ -255,3 +255,327 @@ def make_CNN_model(imgsize, N_classes, complexity = 20):
 
 
 
+# ------- Classification model: Siamese CNN -------
+
+def prep_data_for_SiameseCNN_model(A,imgsize):
+    # Keras implementation of 2d convolutional layers requires that the image has channels, 
+    # even if there's a single channel.
+    return A.reshape(A.shape[0],2,1,imgsize,imgsize)
+
+
+
+def seen_based_probabilities(seen):
+    # Utility for selecting items from an array based on how many times they were seen.
+    # Calculates a normalized probability array weighted towards less-seen elements.
+    # Input: a 1D numpy array of how many times the elements have been seen.
+    p = np.sum(seen+1) / (seen+1)
+    p = p / np.sum(p)
+    return p
+
+
+
+def select_with_seen_based_probabilities(elems,seen,quantity=1,yield_indices=True):
+    # Utility for selecting items from an array based on how many times they were seen.
+    # Randomly selects elements from 'elems', weighted towards less seen elements.
+    # Returns a list of elements, or a numpy array of their indices if 'yield_indices' is true.
+    indices = np.random.choice(np.arange(len(elems)),size=quantity,p=seen_based_probabilities(seen))
+    
+    if quantity == 1:
+        indices = [indices]
+    if not yield_indices:
+        res = []
+    for index in indices:
+        seen[index] += 1
+        if not yield_indices:
+            res.append(elems[index])
+    if yield_indices:
+        res = indices
+    return res
+
+
+
+def select_random_data_pair_indices_for_SiameseCNN(
+    data,      # Array containing data (has to support yielding category indices, like RasterArray)
+    data_seen, # Array containing information about how many times each individual data element was already selected
+    N_classes, # How many classes are there
+    N_pairs_to_select = 600 # How many pairs in total to select (approx.)
+):
+    # Utility for selecting indices of random data pairs
+    # Yields approx. 1:1 ratio of "identical" and "different" data pairs
+    
+    # Generate "random" pairs
+    # Most random pairs will be different
+    # Some random pairs can already be identical, so for 1:1 balance we will need a bit more "random" 
+    # and a bit less "identical" pairs
+    
+    N_random_pairs_in_batch = int(
+        (N_pairs_to_select / 2) * (1 + 1/(N_classes-1))
+    )
+
+    selection_indices_A = select_with_seen_based_probabilities(
+        data,data_seen,
+        quantity=N_random_pairs_in_batch*2,yield_indices=True
+    )
+    selection_indices_B = selection_indices_A[N_random_pairs_in_batch:]
+    selection_indices_A = selection_indices_A[:N_random_pairs_in_batch]
+
+    # Generate "identical" pairs for each class.     
+    N_identical_pairs_in_batch = int(
+        (N_pairs_to_select - N_random_pairs_in_batch)
+    )
+    for category in range(0,N_classes):
+        curr_category_pair_quantity = N_identical_pairs_in_batch//N_classes
+        curr_category_indices = data.category_indices[category]
+        curr_category_indices = np.array(curr_category_indices)
+        selected_curr_category_indices = select_with_seen_based_probabilities(
+            data[curr_category_indices],data_seen[curr_category_indices],
+            quantity=curr_category_pair_quantity*2,yield_indices=True
+        )
+        selection_indices_A_identical = curr_category_indices[selected_curr_category_indices]
+        selection_indices_B_identical = selection_indices_A_identical[curr_category_pair_quantity:]
+        selection_indices_A_identical = selection_indices_A_identical[:curr_category_pair_quantity]
+        selection_indices_A = np.concatenate((selection_indices_A,selection_indices_A_identical),axis=0)
+        selection_indices_B = np.concatenate((selection_indices_B,selection_indices_B_identical),axis=0)
+    
+    return [selection_indices_A,selection_indices_B]
+
+
+
+
+def make_SiameseCNN_model(
+    imgsize, N_classes, 
+    complexity = 16, dense_complexity = 8,
+    CNN_dropout_strength = 0.25,
+    dense_dropout_strength = 0.01
+):
+    
+    
+    # Subroutine for making the CNN subnetwork
+    def make_CNN_part(nameprefix = 'A',complexity = 16,dense_complexity = 8,dropout_strength = 0.25):
+        CNNpart = keras.models.Sequential()
+        CNNpart.add( keras.layers.Conv2D(
+            1*complexity, 5, strides=(1, 1),
+            padding='same', data_format="channels_first",
+            activation='relu', 
+            use_bias=True,
+            kernel_initializer='glorot_uniform', bias_initializer='zeros',
+            kernel_regularizer=None, bias_regularizer=None,
+            activity_regularizer=None,
+            kernel_constraint=None, bias_constraint=None,
+            name='CNN_conv1_'+nameprefix,
+        ))
+
+        CNNpart.add( keras.layers.MaxPooling2D(        
+            pool_size=(2, 2), 
+            strides=None, padding='valid', 
+            data_format="channels_first",
+            name='CNN_maxpool1_'+nameprefix,
+        ))
+
+        CNNpart.add( keras.layers.Dropout(
+            CNN_dropout_strength,
+            name='CNN_dropout1_'+nameprefix,
+        ))
+
+        CNNpart.add( keras.layers.Conv2D(
+            2*complexity, 5, strides=(1, 1),
+            padding='same', data_format="channels_first",
+            activation='relu', 
+            use_bias=True,
+            kernel_initializer='glorot_uniform', bias_initializer='zeros',
+            kernel_regularizer=None, bias_regularizer=None,
+            activity_regularizer=None,
+            kernel_constraint=None, bias_constraint=None,
+            name='CNN_conv2_'+nameprefix,
+        ))
+
+        CNNpart.add( keras.layers.MaxPooling2D(        
+            pool_size=(2, 2), 
+            strides=None, padding='valid', 
+            data_format="channels_first",
+            name='CNN_maxpool2_'+nameprefix,
+        ))
+
+        CNNpart.add( keras.layers.Dropout(
+            CNN_dropout_strength,
+            name='CNN_dropout2_'+nameprefix,
+        )) 
+
+        CNNpart.add( keras.layers.Flatten(
+            name='CNN_finalflat_'+nameprefix,
+        ))
+
+        CNNpart.add( keras.layers.Dense(
+            units=2*complexity*dense_complexity, 
+            activation='relu', 
+            kernel_initializer='glorot_uniform',
+            name='CNN_dense_'+nameprefix,
+        ))
+
+        CNNpart.add( keras.layers.Dropout(
+            CNN_dropout_strength,
+            name='CNN_dense_dropout_'+nameprefix,
+        )) 
+
+        return CNNpart
+
+    # Subroutine for making a Lambda layer which outputs absolute difference of the inputs.
+    def absolute_tensor_difference(pair_of_tensors):
+        x, y = pair_of_tensors
+        return keras.backend.abs(x - y)
+    
+    
+    # ---- Main routine ----
+    
+    if not isinstance(imgsize,tuple):
+        imgsize_for_conv = (imgsize,imgsize)
+    else:
+        imgsize_for_conv = imgsize
+    
+    
+    SCNN_inputA = keras.layers.Input(shape=(1,*imgsize_for_conv),name='inputA')
+    SCNN_inputB = keras.layers.Input(shape=(1,*imgsize_for_conv),name='inputB')
+
+    CNNpart = make_CNN_part(
+        nameprefix = 'A',
+        complexity = complexity,
+        dense_complexity = dense_complexity,
+        dropout_strength = CNN_dropout_strength
+    )
+
+    SCNN_conv_outputA = CNNpart(SCNN_inputA)
+    SCNN_conv_outputB = CNNpart(SCNN_inputB)
+
+    SCNN_abs_difference = keras.layers.Lambda( 
+        absolute_tensor_difference,
+        name='abs_difference',
+    )([SCNN_conv_outputA,SCNN_conv_outputB])
+
+    SCNN_finaldense = keras.layers.Dense(
+        units=2*complexity*dense_complexity, 
+        activation='sigmoid', 
+        name='finaldense',
+    )(SCNN_abs_difference)
+
+    SCNN_finaldense_dropout = keras.layers.Dropout(
+        dense_dropout_strength,
+        name='finaldense_dropout'
+    )(SCNN_finaldense) 
+
+    SCNN_output = keras.layers.Dense(
+        1, 
+        activation='sigmoid',
+        name='output'
+    )(SCNN_finaldense_dropout)
+
+    model = keras.models.Model(inputs=[SCNN_inputA,SCNN_inputB],outputs=[SCNN_output])
+
+    # optimizer = keras.optimizers.RMSprop(0.01)
+    optimizer = keras.optimizers.Adam(0.001)
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer=optimizer,
+        metrics=['accuracy']
+    )
+    
+    return model
+
+
+
+class SiameseCNN_classifier():
+    
+    model = None
+    support_raster_array = None
+    similarity_matrix = None
+    
+    def __init__(self, model, support_raster_array): 
+        self.model = model
+        self.support_raster_array = support_raster_array
+
+    def classify(self, test_image, subsample_size, subsample_attempts=1):        
+        imgsize = self.support_raster_array.imgsize
+        test_image_class_similarities = np.zeros(self.support_raster_array.N_different_categories)
+        for i in range(0,subsample_attempts):
+            # Make a representative subset of the support data array
+            representative_rasters = self.support_raster_array.representative_subsample(subsample_size)
+            # Make an array of image pairs: 
+            #  first image is always the test image, 
+            #  second image is each of the class-representative images
+            test_image_array = np.repeat(test_image[np.newaxis,...], len(representative_rasters), axis=0)
+            X_predict = np.array([test_image_array,representative_rasters.rasters])
+            X_predict = X_predict.transpose(1,0,2,3)
+            X_predict_conv = prep_data_for_SiameseCNN_model(X_predict,imgsize)
+
+            # Feed the array to the model to obtain similarities to each of the class-representative images
+            Y_predicted = self.model.predict(
+                {'inputA': X_predict_conv[:,0], 'inputB': X_predict_conv[:,1]},
+            )
+
+            test_image_pair_similarities = np.array(
+                [representative_rasters.categories,Y_predicted.flatten()]
+            )
+            test_image_pair_similarities = test_image_pair_similarities.transpose()
+            #print(test_image_pair_probabilities)
+
+            test_image_class_similarities += np.mean(
+                Y_predicted.flatten().reshape(-1, subsample_size), 
+                axis=1
+            )
+        
+        return test_image_class_similarities / subsample_attempts
+    
+    
+    def similarity_matrix(self, subsample_size, protect_against_oom=True, oom_protection_limit=100000000):
+        
+        imgsize = self.support_raster_array.imgsize        
+        support_raster_indices_A = []
+        support_raster_indices_B = []
+        
+        # At the moment the pairs are generated and stored in-memory, which can quickly get out of hand
+        # with a large number of categories. Following is a hacky precaution to ensure IPython won't choke
+        if protect_against_oom:
+            memory_estimate = np.square(self.support_raster_array.N_different_categories)
+            memory_estimate = memory_estimate * 2
+            memory_estimate = memory_estimate * imgsize * imgsize
+            if memory_estimate > oom_protection_limit:
+                raise "Similarity matrix calculation liable to run out of memory; reduce subsample size"
+        
+        for category_A in range(0,self.support_raster_array.N_different_categories):
+            for category_B in range(0,self.support_raster_array.N_different_categories):
+                curr_category_A_indices = self.support_raster_array.category_indices[category_A]
+                curr_category_A_indices = np.array(curr_category_A_indices)
+                chosen_indices_A = np.random.choice(curr_category_A_indices, subsample_size, replace=False)
+                curr_category_B_indices = self.support_raster_array.category_indices[category_B]
+                curr_category_B_indices = np.array(curr_category_B_indices)
+                chosen_indices_B = np.random.choice(curr_category_B_indices, subsample_size, replace=False)
+                for index_A in chosen_indices_A:
+                    for index_B in chosen_indices_B:
+                        support_raster_indices_A.append(index_A)
+                        support_raster_indices_B.append(index_B)
+
+        support_raster_indices_A = np.array(support_raster_indices_A)
+        support_raster_indices_B = np.array(support_raster_indices_B)
+        
+        X_predict = np.array(
+            [self.support_raster_array[support_raster_indices_A],
+             self.support_raster_array[support_raster_indices_B]]
+        )
+        print(X_predict.shape)
+        X_predict = X_predict.transpose(1,0,2,3)
+        X_predict_conv = prep_data_for_SiameseCNN_model(X_predict,imgsize)
+
+        Y_predicted = self.model.predict(
+            {'inputA': X_predict_conv[:,0], 'inputB': X_predict_conv[:,1]},
+        )
+        Y_predicted = Y_predicted.flatten()
+        
+        similarity_matrix = np.copy(Y_predicted)
+        # NB: check if reshaping this way is correct
+        similarity_matrix = similarity_matrix.reshape(
+            self.support_raster_array.N_different_categories,
+            self.support_raster_array.N_different_categories,
+            subsample_size,subsample_size
+        )
+        similarity_matrix = similarity_matrix.mean((2,3))
+        self.similarity_matrix = similarity_matrix
+        return [similarity_matrix, Y_predicted]
